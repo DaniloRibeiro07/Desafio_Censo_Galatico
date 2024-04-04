beee1668161bb93c1f4aa1e148606f7b94f8eb4b
Danilo Ribeiro <danilo.ribeirodr01@gmail.com>
2024-04-03 22:06:33 -0300

    Tarefa 3 Concluída

--- index.js
+++ index.js
@@ -17,10 +17,29 @@ async function generatePlanetsButton (){
   planets.forEach(planet => {
     const button = document.createElement('button')
     button.innerText = planet.name
+    const div = document.createElement('div')
     const br = document.createElement('br')
     const br2 = document.createElement('br')
 
-    Array(button, br, br2).forEach(elemnt => {
+    button.onclick = () => {
+      if(div.innerHTML==""){
+        const ul = document.createElement('ul')
+
+        ul.innerHTML= 
+          `
+          <li>Nome: ${planet.name}</li>
+          <li>Clima: ${planet.climate}</li>
+          <li>População: ${planet.population}</li>
+          <li>Terreno: ${planet.terrain}</li>
+          `
+          
+        div.append(ul)
+      }else{
+        div.innerHTML = ""
+      }
+    }
+
+    Array(button, br, div, br2).forEach(elemnt => {
       document.body.appendChild(elemnt)
     })
   });
