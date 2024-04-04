fb6bdf3740696ab6aa92015cb4828d2d9b538667
Danilo Ribeiro <danilo.ribeirodr01@gmail.com>
2024-04-03 21:40:45 -0300

    Tarefa_2 Concluida

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-async function consultPlanetData() {
+async function planetData() {
   const linkAllPlanets = "https://swapi.dev/api/planets/?format=json"
   const request = await fetch(linkAllPlanets)
+
   if(request.ok){
     const jsonResponse = (await request.json()).results
-    console.log (jsonResponse)
+    return (jsonResponse)
   }else{
-    console.log (`Erro na requisição, código: ${request.status}`)
+    return (false)
   }
 }
 
-consultPlanetData()
+async function generatePlanetsButton (){
+  const planets = await planetData()
+  if (!planets) return;
+
+  planets.forEach(planet => {
+    const button = document.createElement('button')
+    button.innerText = planet.name
+    const br = document.createElement('br')
+    const br2 = document.createElement('br')
+
+    Array(button, br, br2).forEach(elemnt => {
+      document.body.appendChild(elemnt)
+    })
+  });
+}
+
+generatePlanetsButton()
